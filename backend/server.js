606a857acc2e9fb0b4cb8f12ad09c75a9d6b97bd
Mufaddal Shakir <muffi606@gmail.com>
2021-05-30 22:07:41 +0500

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,13 @@
-const express = require('express')
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
+
+import productRoutes from './routes/productRoutes.js'
+
+dotenv.config()
+
+connectDB()
 
 const app = express()
 
@@ -7,13 +15,8 @@ app.get('/', (req,res) => {
     res.send('API is running...')
 })
 
-app.get('/api/products', (req,res) => {
-    res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req,res) => {
-    const product = products.find( p => p._id === req.params.id) ;
-    res.json(product)
-})
+const PORT = process.env.PORT || 5000
 
-app.listen(5000, console.log('Server is running on port 5000.'))
+app.listen(PORT, console.log(`Server is running ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold))
