e8e8377da5aeecbf5e8306b452ea45bb9732f103
Mufaddal Shakir <muffi606@gmail.com>
2021-06-01 06:32:36 +0500

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import colors from 'colors'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
+import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
@@ -16,6 +17,14 @@ app.get('/', (req,res) => {
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+
+//@ Making of error handler middleware
+app.use(errorHandler)
+
+
+
 
 const PORT = process.env.PORT || 5000
 
