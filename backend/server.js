0456eb8d200251920033189169dfc196e7b2b501
Mufaddal Shakir <muffi606@gmail.com>
2021-06-03 20:42:17 +0500

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
+
+import productRoutes from './routes/productRoutes.js'
+import {notFound, errorHandler} from './middleware/errorMiddleware.js'
+
+dotenv.config()
+
+connectDB()
+
+const app = express()
+
+app.get('/', (req,res) => {
+    res.send('API is running...')
+})
+
+app.use('/api/products', productRoutes)
+
+app.use(notFound)
+
+//@ Making of error handler middleware
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server is running ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold))
