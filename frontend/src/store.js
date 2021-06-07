70e6d3ba93719458b66fb46d1a4a1d7a5b29bb40
Mufaddal Shakir <muffi606@gmail.com>
2021-06-07 20:14:14 +0500

    Add files via upload

---
+++ frontend/src/store.js
@@ -0,0 +1,23 @@
+import { createStore, combineReducers, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { 
+    productListReducer, 
+    productDetailsReducer 
+} from './reducer/productReducers'
+
+const reducer = combineReducers({
+    productList: productListReducer,
+    productDetails: productDetailsReducer,       
+})
+
+const initialState = {}
+
+const middlware = [thunk]
+
+const store = createStore(
+    reducer, 
+    initialState, 
+    composeWithDevTools(applyMiddleware(...middlware)))
+
+export default store
