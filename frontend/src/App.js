b3d713797dbd4ca2dc7025423271ee384ef8c398
Mufaddal Shakir <muffi606@gmail.com>
2021-06-10 22:18:27 +0500

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 
@@ -15,11 +16,12 @@ const App = () => {
           <Container> 
             <Route path='/' component ={HomeScreen} exact/>
             <Route path='/product/:id' component={ProductScreen}/>
+            <Route path='/cart/:id?' component={CartScreen} />
           </Container>
       </main>
       <Footer />
     </Router>  
-    </>
+    </>    
   );
 }
 
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,13 +5,21 @@ import {
     productListReducer, 
     productDetailsReducer 
 } from './reducer/productReducers'
+import { cartReducer } from './reducer/cartReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
-    productDetails: productDetailsReducer,       
+    productDetails: productDetailsReducer,
+    cart: cartReducer,       
 })
 
-const initialState = {}
+const cartItemsFromStorage = localStorage.getItem('cartItems') 
+    ? JSON.parse(localStorage.getItem('cartItems')) 
+    : []
+
+const initialState = {
+    cart: { cartItems: cartItemsFromStorage }
+}
 
 const middlware = [thunk]
 
