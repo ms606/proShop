08d5451fb0cc238fd719c2b6d16d3a563c9503bb
Mufaddal Shakir <muffi606@gmail.com>
2021-06-14 05:28:48 +0500

    Add files via upload

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 
 const userSchema = mongoose.Schema({
     name: {
@@ -16,11 +17,25 @@ const userSchema = mongoose.Schema({
     },
     isAdmin: {
         type: Boolean,
-        required: true,
+        required: true,   
         default: false
     }
 }, {
     timestamps: true
+})
+
+userSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
+userSchema.pre('save', async function(next) {
+
+    if (!this.isModified('password')){
+        next()
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
 })
 
 const User = mongoose.model('User', userSchema)
--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,10 @@ import colors from 'colors'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+
 import {notFound, errorHandler} from './middleware/errorMiddleware.js'
+
 
 dotenv.config()
 
@@ -12,11 +15,14 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req,res) => {
     res.send('API is running...')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 
