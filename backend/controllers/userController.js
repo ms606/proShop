3cdb3f9ec192a5d5b9fd4a94c89bddf3904cb792
Mufaddal Shakir <muffi606@gmail.com>
2021-06-22 06:34:46 +0500

    Add files via upload

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -88,7 +88,7 @@ const updateUserProfile = asyncHandler(async (req, res) => {
    const user = await User.findById(req.user._id)
  
     if (user) {
-      user.name = req.body.user || user.name
+      user.name = req.body.name || user.name
       user.email = req.body.email || user.email
       
       if (req.body.password) {
