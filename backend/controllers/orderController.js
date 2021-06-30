6bd5b8198c318cc0e00ef8552fd3800450832c38
Mufaddal Shakir <muffi606@gmail.com>
2021-06-30 06:21:37 +0500

    Add files via upload

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,12 +1,11 @@
 import asyncHandler from 'express-async-handler'
 import Order from '../models/orderModel.js'
 
-
 // @desc    Create new order
 // @route   POST /api/orders
-// @access  Private    
+// @access  Private
 const addOrderItems = asyncHandler(async (req, res) => {
-   const {
+  const {
     orderItems,
     shippingAddress,
     paymentMethod,
@@ -14,27 +13,79 @@ const addOrderItems = asyncHandler(async (req, res) => {
     taxPrice,
     shippingPrice,
     totalPrice,
-   } = req.body
+  } = req.body
 
-
-   if (orderItem && orderItems.length === 0 ) {
+  if (orderItems && orderItems.length === 0) {
     res.status(400)
     throw new Error('No order items')
     return
-    } else {
-      const order = new Order({
-          orderItems,
-          user: req.user._id,
-          shippingAddress,
-          paymentMethod,
-          itemsPrice,
-          taxPrice,
-          shippingPrice,
-          totalPrice,
-   })
+  } else {
+    const order = new Order({
+      orderItems,
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    })
 
-   const createdOrder = await order.save()
-   res.status(201).json(createdOrder)
+    const createdOrder = await order.save()
+
+    res.status(201).json(createdOrder)
   }
-}) 
+})
 
+
+// @desc    Get order by ID
+// @route   GET /api/orders/:id
+// @access  Private
+const getOrderById = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id).populate(
+      'user',
+      'name email'
+    )
+
+    if (order) {
+      res.json(order)
+    } else {
+      res.status(404)
+      throw new Error('Order not found')
+    }
+  }
+)
+
+// @desc    Update order to paid
+// @route   GET /api/orders/:id/pay
+// @access  Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+
+  const order = await Order.findById(req.params.id)
+
+  if (order) {
+    order.isPaid = true
+    order.paidAt = Date.now()
+    order.paymentResult = {
+    //   id: req.body.id,
+    //   status: req.body.status,
+         update_time: new Date(),
+    //   email_address: req.body.email_address
+    }
+
+    const updatedOrder = await order.save()
+
+    res.json(updatedOrder)
+
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+}
+)
+
+export {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid
+}
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,12 +1,15 @@
 import express from 'express'
-import {
-    addOrderItems
-} from '../controllers/orderController.js'
-import { protect } from '../middleware/authMiddleware.js'
-
 const router = express.Router()
 
+import {
+    addOrderItems,
+    getOrderById,
+    updateOrderToPaid,
+} from '../controllers/orderController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
+
 router.route('/').post(protect, addOrderItems)
+router.route('/:id').get(protect, getOrderById)
+router.route('/:id/pay').put(protect, updateOrderToPaid)
 
-
-export default { addOrderItems }
+export default router
