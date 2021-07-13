4f7a64c28949899456e05fac112917e0903259d1
Mufaddal Shakir <muffi606@gmail.com>
2021-07-13 23:22:48 +0500

    Add files via upload

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -10,7 +10,6 @@ const getProducts = asyncHandler(async (req, res) => {
     res.json(products)
 }) 
 
-
 // @desc Fetch single product
 // @route GET /api/products/:id
 // @access Public 
@@ -26,7 +25,24 @@ const getProductById = asyncHandler(async (req, res) => {
         }        
 }) 
 
+// @desc   Delete a product
+// @route  DELETE /api/products/:id
+// @access Private/Admin
+const deleteProduct = asyncHandler(async (req, res) => {
+        
+    const product = await Product.findById(req.params.id)
+    
+    if (product) {
+        await product.remove()
+        res.json({ message: 'Product removed'})
+    } else {
+        res.status(404)
+        throw new Error('Product not found')
+    }        
+}) 
+
 export {
     getProducts,
-    getProductById
+    getProductById,
+    deleteProduct
 }
