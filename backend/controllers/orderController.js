2ece1e476c2c2c217c6e8ff72aac067e88d4ef3c
Mufaddal Shakir <muffi606@gmail.com>
2021-07-17 06:14:39 +0500

    Add files via upload

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -81,8 +81,27 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
     res.status(404)
     throw new Error('Order not found')
   }
-}
-)
+})
+
+// @desc    Update order to delivered
+// @route   PUT /api/orders/:id/deliver
+// @access  Private/Admin
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id)
+
+  if (order) {
+    order.isDelivered = true
+    order.deliveredAt = Date.now()
+    
+    const updatedOrder = await order.save()
+
+    res.json(updatedOrder)
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+})
+
 
 // @desc    Get logged in user orders
 // @route   GET /api/orders/myorders
@@ -92,9 +111,19 @@ const getMyOrders = asyncHandler(async (req, res) => {
   res.json(orders)
 })
 
+// @desc    Get all orders
+// @route   GET /api/orders
+// @access  Private/Admin
+const getOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({}).populate('user', 'id name')
+  res.json(orders)
+})
+
 export {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
-  getMyOrders
+  updateOrderToDelivered,
+  getMyOrders,
+  getOrders
 }
