433a807dccae1186894d2061365afbeb44f8b0a5
Mufaddal Shakir <muffi606@gmail.com>
2021-07-27 21:54:34 +0500

    Add files via upload

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,16 +4,27 @@ import {
         getProducts,
         deleteProduct,
         createProduct,
-        updateProduct
+        updateProduct,
+        createProductReview,
+        getTopProducts,
        } from '../controllers/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
-router.route('/').get(getProducts).post(protect, admin, createProduct) 
+router.route('/')
+        .get(getProducts)
+        .post(protect, admin, createProduct) 
+router.route('/top')
+        .get(getTopProducts)
 router.route('/:id')
         .get(getProductById)    
         .delete(protect, admin, deleteProduct)
         .put(protect, admin, updateProduct)
+
+router.route('/:id/reviews').post(protect, createProductReview)
+
+
+//router.get('/top', getTopProducts)
 
 export default router  
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import morgan from 'morgan'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
@@ -14,8 +15,12 @@ import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 dotenv.config()
 
 connectDB()
-
+  
 const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
 
 app.use(express.json())
 
